4e030f6132d0120335a25b47b74a2b80c3b95739
xClouder <xclouder@qq.com>
2017-09-10 22:44:10 +0800

    import entities and add demo system

---
+++ Assets/Game/Components/DebugMessageComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+
+[GameAttribute]
+public class DebugMessageComponent : IComponent {
+
+
+	public string message;
+
+
+}
---
+++ Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+
+public class GameController : MonoBehaviour {
+
+	Systems _systems;
+
+	void Start()
+	{
+		var contexts = Contexts.sharedInstance;
+
+		_systems = new Feature("Systems")
+			.Add(new DebugMessageSystem(contexts))
+			.Add(new HelloWorldSystem(contexts));
+		
+
+		_systems.Initialize();
+	}
+
+	void Update()
+	{
+		// call Execute() on all the IExecuteSystems and 
+		// ReactiveSystems that were triggered last frame
+		_systems.Execute();
+		// call cleanup() on all the ICleanupSystems
+		_systems.Cleanup();
+	}
+
+}
