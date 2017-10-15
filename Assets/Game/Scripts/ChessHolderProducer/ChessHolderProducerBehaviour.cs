65a52aed8b07976b9a7199100a5146e42c28967b
xClouder <xclouder@qq.com>
2017-10-15 17:00:29 +0800

    棋盘布置

---
+++ Assets/Game/Scripts/ChessHolderProducer/ChessHolderProducerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChessHolderProducerBehaviour : MonoBehaviour {
+
+	public int round;
+	public int pos;
+
+	// Use this for initialization
+	void Start () {
+		var e = Contexts.sharedInstance.game.CreateEntity();
+		e.AddPosition(round, pos);
+		e.isChessPieceHolder = true;
+	}
+}
---
+++ Assets/Game/Systems/AddChessHolderSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+using Entitas.Unity;
+
+public class AddChessHolderSystem : ReactiveSystem<GameEntity> {
+
+	readonly Transform _viewContainer = new GameObject("ChessHolder Views").transform;
+	readonly GameContext _context;
+
+	public AddChessHolderSystem(Contexts contexts) : base(contexts.game)
+	{
+		_context = contexts.game;
+	}
+
+	protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+	{
+		return context.CreateCollector(GameMatcher.ChessPieceHolder);
+	}
+
+	protected override bool Filter(GameEntity entity)
+	{
+		return !entity.isChessPieceHolder;
+	}
+
+	protected override void Execute(List<GameEntity> entities)
+	{
+		foreach (GameEntity e in entities)
+		{
+			GameObject go = new GameObject("Chess Holder View");
+			go.transform.SetParent(_viewContainer, false);
+			e.AddView(go);
+			go.Link(e, _context);
+		}
+	}
+
+}
