593fdf8ccd29f58ed7965bdb8b5dc091404bb9d7
xClouder <xclouder@qq.com>
2017-10-19 08:49:45 +0800

    partial gamelogic system

--- Assets/Game/Systems/GameLogicSystem.cs
+++ Assets/Game/Systems/GameLogicSystem.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Entitas;
 
-public class GameLogicSystem : MonoBehaviour {
+public class GameLogicSystem : ReactiveSystem<InputEntity> {
 
-	// Use this for initialization
-	void Start () {
-	
+	private GameContext _gameContext;
+
+	public GameLogicSystem(Contexts contexts) : base(contexts.input)
+	{
+		_gameContext = contexts.game;
+
+		_gameContext.GetGroup(GameMatcher.ChessPiece).OnEntityAdded += OnAddChessPiece;
+		_gameContext.GetGroup(GameMatcher.ChessPiece).OnEntityRemoved += OnRemoveChessPiece;
+		_gameContext.GetGroup(GameMatcher.ChessPiece).OnEntityUpdated += OnUpdateChessPiece;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void OnAddChessPiece(IGroup<GameEntity> grp, GameEntity entity, int index, IComponent comp)
+	{
+		//add chess piece
+
 	}
+
+	private void OnRemoveChessPiece(IGroup<GameEntity> grp, GameEntity entity, int index, IComponent comp)
+	{
+
+	}
+
+	private void OnUpdateChessPiece(IGroup<GameEntity> grp, GameEntity entity, int index, IComponent comp)
+	{
+
+	}
+
+	protected override ICollector<InputEntity> GetTrigger(IContext<InputEntity> context)
+	{
+		return context.CreateCollector(InputMatcher.AnyOf(InputMatcher.SelectChessHolder));
+	}
+
+	protected override bool Filter (InputEntity entity)
+	{
+		return entity.hasSelectChessHolder;
+	}
+
+	protected override void Execute (System.Collections.Generic.List<InputEntity> entities)
+	{
+		
+	}
+
+
 }
